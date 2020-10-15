54088a4de4b751cb40ce5f4f70f4ff553214f911
jiin-yang <jinyang.sv.69.@gmail.com>
2020-10-15 14:17:00 +0300

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Server/Server.h"
 int main() {
+	//std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     SocketServer::Server* server = new SocketServer::Server("localhost", 3001);
     server->startServer();
